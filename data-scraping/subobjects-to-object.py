928f80e6fe6a19d883aa423f03085389d8c2bf32
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-02-09 23:36:45 -0500

    added data with reviews in json

--- data-scraping/subobjects-to-object.py
+++ data-scraping/subobjects-to-object.py
@@ -5,12 +5,6 @@ def load_file_contents():
         contents = file.read()
     return contents
 
-data = json.loads(load_file_contents())
-merged_data = {}
-
-flattened_data = [item for sublist in data for item in sublist]
-print(flattened_data)
-
 def save_to_file(data):
     with open('data-fixed.json', 'r+') as file:
         existing_data = json.load(file)
@@ -18,4 +12,8 @@ def save_to_file(data):
         file.seek(0)
         json.dump(existing_data, file, indent=2)
 
+data = json.loads(load_file_contents())
+merged_data = {}
+
+flattened_data = [item for sublist in data for item in sublist]
 save_to_file(flattened_data)
