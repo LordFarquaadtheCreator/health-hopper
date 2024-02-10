f915d775708838a174ee06780b7c27b07d27f199
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-02-09 21:51:58 -0500

    data-fixed.json has cleanly listed data

--- data-scraping/google-review-scraper.py
+++ data-scraping/google-review-scraper.py
@@ -6,9 +6,10 @@ driver = webdriver.Firefox()
 # Goto website
 website = r"https://www.google.com/maps/place/Magic+Smile+Turkey/@36.8566468,30.7806438,17z/data=!3m1!4b1!4m6!3m5!1s0x14c39bd50dbec82f:0x2d1bffc12eae1ed4!8m2!3d36.8566425!4d30.7832187!16s%2Fg%2F11h7c_91s_?authuser=2&entry=ttu"
 driver.get(website)
-xpat
+xpath = r"/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]"
+
 try:
-    title = driver.find_element(by="xpath", value=r"/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]")
+    title = driver.find_element(by="xpath", value=xpath)
     if (title):
         print("Element found")
         title.click()
