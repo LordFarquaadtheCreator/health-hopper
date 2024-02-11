c997323b32291cff028e9b59b98a349692ef86bb
Ayan Das <adas006@citymail.cuny.edu>
2024-02-11 06:31:10 -0500

    finalizing changes, added 3 additional models, don't have time to display the information on the client side, on the server side, the resulting outputs and graphs are stored in the Text_Files and visualization directories, will need to work on pitch and submission now

--- server/Machine-Learning/keyword_extraction.py
+++ server/Machine-Learning/keyword_extraction.py
@@ -1,6 +1,6 @@
 # this file will be used to extract the keywords that was used to cateogirze the sentiment analysis model, this will utilize a seperate machine learning model of it's own
 # this file will be used to extract the keywords that was used to categorize the sentiment analysis model
-from sentiment_analysis import all_companies_sentiments, review_snippets_2d
+from sentiment_analysis1 import all_companies_sentiments, review_snippets_2d
 import os
 import yake
 
