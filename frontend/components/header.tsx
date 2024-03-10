fd2a2b5a24f4dbee36c0c5f10a569c8db359802e
garvsl <garvsehgal7@gmail.com>
2024-03-10 17:27:03 -0400

    fix bottom border

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -14,29 +14,31 @@ import { Sheet, SheetTrigger } from "./ui/sheet";
 
 export default function Header() {
   return (
-    <div className="flex py-4 flex-row items-center justify-between border-b ml-[auto] mr-[auto] px-6 [maxWidth:_clamp(1200px,70vw,1400px)]">
-      <Link href="/" className="flex flex-row items-center gap-[4px] ">
-        <h1 className="font-light tracking-tighter  subpixel-antialiased     text-2xl">
-          HealthHopper
-        </h1>
-      </Link>
+    <div className=" border-b ">
+      <div className="flex py-4 flex-row items-center justify-between ml-[auto] mr-[auto] px-6 [maxWidth:_clamp(1200px,70vw,1400px)]">
+        <Link href="/" className="flex flex-row items-center gap-[4px] ">
+          <h1 className="font-light tracking-tighter  subpixel-antialiased     text-2xl">
+            HealthHopper
+          </h1>
+        </Link>
 
-      <div className="flex flex-row items-center gap-2">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline">
-              <UploadIcon />
-            </Button>
-          </SheetTrigger>
-          <SheetDemo />
-        </Sheet>
-        <Button variant="ghost">
-          <BellIcon className="w-4 h-4" />
-        </Button>
-        <ThemeChange />
-        <Button variant={"link"} className="tracking-tight font-medium">
-          <Link href={"/auth/login"}>Sign in</Link>
-        </Button>
+        <div className="flex flex-row items-center gap-2">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline">
+                <UploadIcon />
+              </Button>
+            </SheetTrigger>
+            <SheetDemo />
+          </Sheet>
+          <Button variant="ghost">
+            <BellIcon className="w-4 h-4" />
+          </Button>
+          <ThemeChange />
+          <Button variant={"link"} className="tracking-tight font-medium">
+            <Link href={"/auth/login"}>Sign in</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
