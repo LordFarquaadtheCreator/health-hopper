063db4f0aab1048342029399fc7722adc050c12c
garvsl <garvsehgal7@gmail.com>
2024-03-16 01:29:19 -0400

    not workin

--- frontend/components/cardItem.tsx
+++ frontend/components/cardItem.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Skeleton, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
 export const CardItem = ({
   className,
@@ -11,15 +12,21 @@ export const CardItem = ({
   return (
     <div
       className={cn(
-        " rounded-xl flex-shrink-0  flex-0 min-w-[400px] h-[300px] group/bento transition duration-200  p-4 dark:bg-black dark:border-white/[0.2] bg-white border  justify-between flex flex-col",
+        " rounded-xl flex-shrink-0  flex-0 min-w-[400px] h-[300px] group/bento transition duration-200   dark:bg-black dark:border-white/[0.2] bg-white border  justify-between flex flex-col",
         className
       )}
     >
-      <Skeleton className="w-full h-full" />
-      <div className="group-hover/bento:translate-x-2 transition duration-200 flex flex-col">
+      <Image
+        className="rounded-t-xl "
+        alt=""
+        width={400}
+        height={225}
+        src={header}
+      />
+      <div className=" p-4 pt-0 group-hover/bento:translate-x-2 transition duration-200 flex flex-col">
         <Text className="text-xs">{rating}/5</Text>
 
-        <Text className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 -mt-1">
+        <Text className="font-sans font-bold  dark:text-neutral-200 mb-2 -mt-1">
           {title}
         </Text>
 
