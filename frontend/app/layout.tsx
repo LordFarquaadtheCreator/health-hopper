ee36a69ece9fcdf647a13ecd21cd94735b3be568
garvsl <garvsehgal7@gmail.com>
2024-03-16 01:32:30 -0400

    get analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,6 +24,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Analytics />
           {/* perhaps this causes blurriness */}
           {/* <div className="ml-[auto] mr-[auto] px-6 [maxWidth:_clamp(1200px,70vw,1400px)]"> */}
           {children}
