398300ea6aa4722c312b1382b11dd41616351322
garvsl <garvsehgal7@gmail.com>
2024-03-17 18:12:08 -0400

    fix big image design

--- frontend/components/home/body/bigImage.tsx
+++ frontend/components/home/body/bigImage.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 import { Button } from "../../ui/button";
 
 export const BigImage = ({ img, text }: any) => {
-  const imageClass = "border rounded-3xl w-full h-[54vh] object-cover";
+  const imageClass = "border rounded-3xl w-full h-[48vh] object-cover";
   return (
     <Flex className="relative">
-      <div className="top-0 p-8 flex flex-col absolute text-left small:w-[50%] w-full">
+      <div className="top-0 p-8 flex flex-col absolute text-left small:w-[65%] w-full">
         <Heading
-          style={{ lineHeight: "1" }}
-          className="[fontSize:_clamp(3rem,5vw,4rem)] text-left tracking-tight font-bold text-white subpixel-antialiased drop-shadow-[0_1.2px_1.2px_rgba(0,0,50,0.4)] text-pretty"
+          style={{ lineHeight: "0.9" }}
+          className="[fontSize:_clamp(2.5rem,5vw,3.5rem)]  text-left tracking-tight font-bold text-white subpixel-antialiased drop-shadow-[0_1.2px_1.2px_rgba(0,0,50,0.4)] text-balance"
         >
-          Find out the perfect clinic for you with our AI integration!
+          {text}
         </Heading>
         <Button className="mt-4 p-6 w-[200px] text-md">Try it out now</Button>
       </div>
